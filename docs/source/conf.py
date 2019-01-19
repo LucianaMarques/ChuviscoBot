74b91d04c15a55ba9a54475daabd1302e84e3446
luciana <lucianadacostamarques@gmail.com>
2019-01-19 16:48:04 -0200

    conserta typo em recommonmark.parser

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-from recommonpark.parser import CommonMarkParser
+from recommonmark.parser import CommonMarkParser
 
 
 # -- Project information -----------------------------------------------------
